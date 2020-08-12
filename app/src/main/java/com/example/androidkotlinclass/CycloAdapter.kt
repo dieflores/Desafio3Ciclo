e38cbc2bdc5ce305c933a68bd223c7e254f4b890
dieflores <labestia012345>
2020-08-12 15:57:46 -0400

    primer commit pero charcha

---
+++ app/src/main/java/com/example/androidkotlinclass/CycloAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.androidkotlinclass
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CycloAdapter {
+
+
+class CycloViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+
+}
+
+
+}
